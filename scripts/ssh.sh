15da13c5ea0593cf1c0ddb1bda8eb115b18ec77f
LuisCalvo <luis122448@gmail.com>
2024-12-15 19:59:57 -0500

    feat: update ssh scripts for deployment

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+source /srv/developer-server/scripts/functions.sh
 source /etc/environment
 
 # Define the variables
+IP_ADDRESS=$(get_config_value "$DEVICE_NAME" "IP")
 REMOTE_SERVER=$SERVER_LOCAL_USER@$SERVER_LOCAL_IP
 CONFIG_FILE="$HOME/.ssh/config"
 SSH_KEY_PATH="$HOME/.ssh/id_rsa"
 PUBLIC_KEY_PATH="$SSH_KEY_PATH.pub"
+GITHUB_KEYS_DIR="$HOME/.ssh/github_keys"
+
+# Validate environment variables
+if [ -z "$SERVER_LOCAL_USER" ] || [ -z "$SERVER_LOCAL_IP" ]; then
+    echo "The environment variables SERVER_LOCAL_USER and SERVER_LOCAL_IP must be set."
+    exit 1
+fi
+
+if [ -z "$EMAIL" ]; then
+    echo "The environment variable EMAIL must be set."
+    exit 1
+fi
 
 if [ ! -f "$SSH_KEY_PATH" ]; then
     ssh-keygen -t rsa -b 4096 -C "$EMAIL" -f "$SSH_KEY_PATH" -N ""
@@ -27,5 +41,7 @@ else
     echo "The configuration for $REMOTE_SERVER already exists in $CONFIG_FILE."
 fi
 
-echo "Copying the SSH key to the remote server $REMOTE_SERVER..."
+echo "Copying the SSH key to the remote server $REMOTE_SERVER"
 ssh-copy-id -i "$PUBLIC_KEY_PATH" "$REMOTE_SERVER"
+ssh "$SERVER_LOCAL_USER@$SERVER_LOCAL_IP" "mkdir -p $GITHUB_KEYS_DIR"
+scp "$PUBLIC_KEY_PATH" "$SERVER_LOCAL_USER@$SERVER_LOCAL_IP:$GITHUB_KEYS_DIR/key_$IP_ADDRESS.pub"
