d4f1fa8c342fbb79a89fbf55a6c842147e80598f
luis122448 <luis122448@gmail.com>
2025-04-05 19:42:30 -0500

    fix: update generate clients vpn again

---
+++ vpn/generate_ovpn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CLIENT="$1"
+
+# Generar certificados con EasyRSA
+cd /etc/openvpn/easy-rsa
+./easyrsa build-client-full "$CLIENT" nopass
+
+# Crear el archivo .ovpn concatenando los archivos necesarios
+cat /etc/openvpn/client-common.txt \
+    <(echo -e '<ca>') /etc/openvpn/easy-rsa/pki/ca.crt <(echo -e '</ca>\n<cert>') \
+    /etc/openvpn/easy-rsa/pki/issued/"$CLIENT".crt <(echo -e '</cert>\n<key>') \
+    /etc/openvpn/easy-rsa/pki/private/"$CLIENT".key <(echo -e '</key>\n<tls-auth>') \
+    /etc/openvpn/tls-auth.key <(echo -e '</tls-auth>') \
+    > /etc/openvpn/"$CLIENT".ovpn
+
+# Cambiar permisos del archivo .ovpn
+chmod 644 /etc/openvpn/"$CLIENT".ovpn
