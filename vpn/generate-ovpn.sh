1c7ddfa1902a1e4cf8878514340ab81fb819fa09
luis122448 <luis122448@gmail.com>
2025-05-03 20:54:18 -0500

    fix: fix crt and key certificate for client vpn again x5

--- vpn/generate-ovpn.sh
+++ vpn/generate-ovpn.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 CLIENT="$1"
-# Ajusta la ruta del PKI según la estructura de EasyRSA 3.x
+# Adjust the PKI path according to EasyRSA 3.x structure
 PKI="/etc/easy-rsa/pki"
 
-# Ir al directorio de EasyRSA para ejecutar los comandos
+sudo chown -R $USER:$USER /etc/easy-rsa
+
+# Go to the EasyRSA directory to run the commands
 cd /etc/easy-rsa
 
-# Generar el certificado y clave para el cliente
+# Generate the certificate and key for the client
+source vars
+
 easyrsa --batch build-client-full "$CLIENT" nopass
 
-# Concatenar los archivos necesarios para crear el archivo .ovpn del cliente
+# Concatenate the necessary files to create the client .ovpn file
 cat /etc/openvpn/client-common.txt \
     <(echo -e '<ca>') \
     "$PKI/ca.crt" \
@@ -23,5 +27,5 @@ cat /etc/openvpn/client-common.txt \
     <(echo -e '</tls-crypt>') \
     > /etc/openvpn/client/"$CLIENT".ovpn
 
-# Establecer permisos de lectura para el archivo .ovpn
+# Set read permissions for the .ovpn file
 chmod 644 /etc/openvpn/client/"$CLIENT".ovpn
