0f43013b41c5676086b5714cd5587bdbd398e90d
luis122448 <luis122448@gmail.com>
2025-05-03 21:05:10 -0500

    fix: fix crt and key certificate for client vpn again x7

--- vpn/generate-ovpn.sh
+++ vpn/generate-ovpn.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
+set -euxo pipefail
 
+if [[ -z "${1-}" ]]; then
+  echo "Usage: $0 <client_name>"
+  exit 1
+fi
 CLIENT="$1"
-# Adjust the PKI path according to EasyRSA 3.x structure
-PKI="/etc/easy-rsa/pki"
 
-# Go to the EasyRSA directory to run the commands
-cd /etc/easy-rsa
+# Paths
+EASYRSA_DIR=/etc/easy-rsa
+PKI_DIR="$EASYRSA_DIR/pki"
+OUT_DIR=/etc/openvpn/client
 
-# Generate the certificate and key for the client
-easyrsa --batch build-client-full "$CLIENT" nopass
+# (Optional) redirect all output to a logfile:
+# exec >"/var/log/easyrsa-${CLIENT}.log" 2>&1
 
-# Concatenate the necessary files to create the client .ovpn file
+cd "$EASYRSA_DIR"
+
+# Initialize PKI if needed:
+# bash ./easyrsa init-pki
+
+# Load Easy-RSA variables
+if [[ -f vars ]]; then
+  # shellcheck disable=SC1091
+  source ./vars
+fi
+
+# Build the client certificate (no password)
+bash ./easyrsa --batch build-client-full "$CLIENT" nopass
+
+# Create the .ovpn file by concatenating all parts
 cat /etc/openvpn/client-common.txt \
-    <(echo -e '<ca>') \
-    "$PKI/ca.crt" \
-    <(echo -e '</ca>\n<cert>') \
-    "$PKI/issued/$CLIENT.crt" \
-    <(echo -e '</cert>\n<key>') \
-    "$PKI/private/$CLIENT.key" \
-    <(echo -e '</key>\n<tls-crypt>') \
-    /etc/openvpn/tls-crypt.key \
-    <(echo -e '</tls-crypt>') \
-    > /etc/openvpn/client/"$CLIENT".ovpn
+  <(echo -e '<ca>') \
+  "$PKI_DIR/ca.crt" \
+  <(echo -e '</ca>\n<cert>') \
+  "$PKI_DIR/issued/$CLIENT.crt" \
+  <(echo -e '</cert>\n<key>') \
+  "$PKI_DIR/private/$CLIENT.key" \
+  <(echo -e '</key>\n<tls-crypt>') \
+  /etc/openvpn/tls-crypt.key \
+  <(echo -e '</tls-crypt>') \
+  > "$OUT_DIR/$CLIENT.ovpn"
 
-# Set read permissions for the .ovpn file
-chmod 644 /etc/openvpn/client/"$CLIENT".ovpn
+# Set permissions
+chmod 644 "$OUT_DIR/$CLIENT.ovpn"
+
+echo "Client '$CLIENT' configuration written to $OUT_DIR/$CLIENT.ovpn"
